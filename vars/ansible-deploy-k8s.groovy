537d45cf04cc3c3f7eb76e059bcf8e3fbf7407b8
Santosh Bitra <60322332+santosh-bitra@users.noreply.github.com>
2022-06-18 19:24:04 +0530

    Update ansible-deploy-k8s.groovy

--- vars/ansible-deploy-k8s.groovy
+++ vars/ansible-deploy-k8s.groovy
@@ -2,6 +2,6 @@
 
 def call(String dockerImage) {
     sh """
-        ansible-playbook deploy_k8s.yml -e ansible_python_interpreter=/home/jenkins/workspace/ansible_venv/bin/python --extra-vars \"image_id=${dockerImage}"
+        ansible-playbook deploy.yml -e ansible_python_interpreter=/home/jenkins/workspace/ansible_venv/bin/python --extra-vars \"image_id=${dockerImage}"
     """
 }
